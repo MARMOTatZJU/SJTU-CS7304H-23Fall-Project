962c362ea5ee92483e8d8a8e3780ea7a6af9bdae
Yinda XU <ydxu_zju@hotmail.com>
2024-01-08 23:17:23 +0800

    [xyd/utils] add utils to export submission

--- common/utils.py
+++ common/utils.py
@@ -1,4 +1,7 @@
+import os
+
 import numpy as np
+import pandas as pd
 
 
 def compute_pseudo_determinant(mat):
@@ -10,3 +13,18 @@ def compute_pseudo_determinant(mat):
 
 def argmedian(x):
     return np.argpartition(x, len(x) // 2)[len(x) // 2]
+
+
+def export_result_for_submission(predicted_labels : np.array, path : str):
+    export_df = pd.DataFrame(
+        data=predicted_labels.astype(np.int64),
+        columns=('label',),
+        )
+    export_df.index.name = 'ID'
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    export_df.to_csv(
+        path,
+        header=True,
+        index=True,
+        )
+    print(f'Result exported to: {path}.')
