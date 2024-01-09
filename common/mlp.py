080d6647c67b6cd8be1f3060e13783021e97ff7f
Yinda XU <ydxu_zju@hotmail.com>
2024-01-09 14:54:44 +0800

    [xyd/mlp] fix mlp issues
    - copy MLP definition from a future version
    - resolve issue related to `pinned_memory`

--- common/mlp.py
+++ common/mlp.py
@@ -1,7 +1,56 @@
+from typing import List, Optional, Callable
 import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin
 import torch
 import torchvision
+
+
+class MLP(torch.nn.Sequential):
+    """This block implements the multi-layer perceptron (MLP) module.
+
+    NOTE: imported from `torchvision.ops.MLP` in 0.16.1, a future version
+
+    Args:
+        in_channels (int): Number of channels of the input
+        hidden_channels (List[int]): List of the hidden channel dimensions
+        norm_layer (Callable[..., torch.nn.Module], optional): Norm layer that will be stacked on top of the linear layer. If ``None`` this layer won't be used. Default: ``None``
+        activation_layer (Callable[..., torch.nn.Module], optional): Activation function which will be stacked on top of the normalization layer (if not None), otherwise on top of the linear layer. If ``None`` this layer won't be used. Default: ``torch.nn.ReLU``
+        inplace (bool, optional): Parameter for the activation layer, which can optionally do the operation in-place.
+            Default is ``None``, which uses the respective default values of the ``activation_layer`` and Dropout layer.
+        bias (bool): Whether to use bias in the linear layer. Default ``True``
+        dropout (float): The probability for the dropout layer. Default: 0.0
+    """
+
+    def __init__(
+        self,
+        in_channels: int,
+        hidden_channels: List[int],
+        norm_layer: Optional[Callable[..., torch.nn.Module]] = None,
+        activation_layer: Optional[Callable[..., torch.nn.Module]] = torch.nn.ReLU,
+        inplace: Optional[bool] = None,
+        bias: bool = True,
+        dropout: float = 0.0,
+    ):
+        # The addition of `norm_layer` is inspired from the implementation of TorchMultimodal:
+        # https://github.com/facebookresearch/multimodal/blob/5dec8a/torchmultimodal/modules/layers/mlp.py
+        params = {} if inplace is None else {"inplace": inplace}
+
+        layers = []
+        in_dim = in_channels
+        for hidden_dim in hidden_channels[:-1]:
+            layers.append(torch.nn.Linear(in_dim, hidden_dim, bias=bias))
+            if norm_layer is not None:
+                layers.append(norm_layer(hidden_dim))
+            layers.append(activation_layer(**params))
+            layers.append(torch.nn.Dropout(dropout, **params))
+            in_dim = hidden_dim
+
+        layers.append(torch.nn.Linear(in_dim, hidden_channels[-1], bias=bias))
+        layers.append(torch.nn.Dropout(dropout, **params))
+
+        super().__init__(*layers)
+        # _log_api_usage_once(self)  # NOTE: available only in future version
+
 
 
 class MultiLayerPerceptronClassifier(BaseEstimator, ClassifierMixin):
@@ -42,15 +91,15 @@ class MultiLayerPerceptronClassifier(BaseEstimator, ClassifierMixin):
             int(2**(base_channels - i_layer))
             for i_layer in range(self.n_layers)
             ] + [C,]
-        self.model = torchvision.ops.MLP(
+        self.model = MLP(
             in_channels=int(D),
             hidden_channels=hidden_channels,
         ).to(device)
         # print(self.model)
 
         dataset = torch.utils.data.TensorDataset(
-            torch.from_numpy(X).type(torch.float32).to(device),
-            torch.from_numpy(y).type(torch.int64).to(device),
+            torch.from_numpy(X).type(torch.float32),
+            torch.from_numpy(y).type(torch.int64),
         )
         data_loader = torch.utils.data.DataLoader(
             dataset,
@@ -68,6 +117,8 @@ class MultiLayerPerceptronClassifier(BaseEstimator, ClassifierMixin):
             for i_iteration, data in enumerate(data_loader):
                 optimizer.zero_grad()
                 batch_X, batch_y = data
+                batch_X = batch_X.to(device)
+                batch_y = batch_y.to(device)
                 logits = self.model(batch_X)
                 loss = criterion(logits, batch_y)
                 loss.backward()
